8737aea69ea8c767784747e0e896f3a39db30086
Gabriel Attorri <gattorri@omnitronic.com.ar>
2020-02-12 11:15:01 -0300

    Modulo 2 completo

---
+++ src/QuizQuestion.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class QuizQuestion extends Component {
+  render() {
+    return (
+      <div>
+        <main>
+          <section>
+            <p>{this.props.quiz_questions.instruction_text}</p>
+          </section>
+          <section className="buttons">
+            <ul>
+              <li>{this.props.quiz_questions.answer_options[0]}</li>
+            </ul>
+          </section>
+        </main>
+      </div>
+    );
+  }
+}
+
+export default QuizQuestion;
