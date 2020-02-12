b3969354e69693145bd58c04cd4ac5a790a34f96
Gabriel Attorri <gattorri@omnitronic.com.ar>
2020-02-12 11:50:08 -0300

    Modulo 3.2 completo

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -4,7 +4,7 @@ class QuizQuestionButton extends Component {
   render() {
     return (
       <li>
-        <button>{this.props.QuizQuestionButton}</button>
+        <button>{this.props.button_text}</button>
       </li>
     );
   }
