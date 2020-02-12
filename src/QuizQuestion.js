90bf56dfab56158f4467f30f814568b10fbb575a
Gabriel Attorri <gattorri@omnitronic.com.ar>
2020-02-12 16:39:04 -0300

    Modulo 7 completo

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -5,17 +5,15 @@ import QuizEnd from "./QuizEnd";
 class QuizQuestion extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      incorrect_answer: false
-    };
+    this.state = { incorrectAnswer: false };
   }
 
   handleClick(buttonText) {
     if (buttonText === this.props.quiz_question.answer) {
-      this.setState({ incorrect_answer: false });
+      this.setState({ incorrectAnswer: false });
       this.props.showNextQuestionHandler();
     } else {
-      this.setState({ incorrect_answer: true });
+      this.setState({ incorrectAnswer: true });
     }
   }
 
@@ -41,7 +39,7 @@ class QuizQuestion extends Component {
               )}
             </ul>
           </section>
-          {this.state.incorrect_answer ? (
+          {this.state.incorrectAnswer ? (
             <p className="error">Sorry, esta MAL</p>
           ) : null}
         </main>
